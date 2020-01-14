6ada582c425ae024afdc8c382daf86d547da2a70
Ali Aqdas <37294680+Ali-Aqdas@users.noreply.github.com>
2020-01-15 00:39:58 +0500

    second commit (2nd lecture)
    firebase added in project and user create in authentication at firebase

--- ChatApp/ViewController.swift
+++ ChatApp/ViewController.swift
@@ -7,14 +7,30 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func signUp(_ sender: Any) {
+        let email = emailTextField.text!
+        let password = passwordTextField.text!
+        Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+            if error == nil {
+                print("success")
+            }else {
+                print(error?.localizedDescription)
+            }
+        }
+    }
+    
 
 }
 
